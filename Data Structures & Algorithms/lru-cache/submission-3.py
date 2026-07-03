a752b3253df29a2e712d56980f3f1f6162c3c83a
Jaem Dessources <jaem.developer@gmail.com>
2026-07-03 00:10:37 -0400

    Add: lru-cache - submission-3

---
+++ Data Structures & Algorithms/lru-cache/submission-3.py
@@ -0,0 +1,44 @@
+class ListNode:
+    def __init__(self, key=None, val=None, next=None, prev=None):
+        self.key, self.val = key, val
+        self.next, self.prev = next, prev
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.hm = {}
+        self.head, self.tail = ListNode(), ListNode()
+        self.head.next, self.tail.prev = self.tail, self.head
+
+    def get(self, key: int) -> int:
+        if key in self.hm:
+            node = self.hm[key]
+            self._remove(node)
+            self._insert(node)
+            return node.val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.hm:
+            node = self.hm[key]
+            self._remove(node)
+            self._insert(node)
+            node.val = value
+        else:
+            if len(self.hm) == self.cap:
+                #evict list recently used (LRU)
+                lru = self.head.next
+                del self.hm[lru.key]
+                self._remove(lru)
+            node = ListNode(key,value)
+            self._insert(node)
+            self.hm[key] = node
+
+    def _insert(self, node: ListNode) -> None:
+        prev, nxt = self.tail.prev, self.tail
+        node.next, node.prev = nxt, prev
+        prev.next= nxt.prev = node 
+
+    def _remove(self, node: ListNode) -> None:
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
