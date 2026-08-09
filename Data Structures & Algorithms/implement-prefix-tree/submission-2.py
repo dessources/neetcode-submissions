43f15b5eb7e8d6809c5269d2c56609c1ff9ec497
Jaem Dessources <jaem.developer@gmail.com>
2026-08-08 20:26:05 -0400

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.py
@@ -0,0 +1,27 @@
+class PrefixTree:
+    def __init__(self):
+        self.hm = {}
+
+    def insert(self, word: str) -> None:
+        cur = self.hm
+        for c in word:
+            if c not in cur:
+                cur[c] = {}
+            cur = cur[c]
+        cur["."] = True
+
+    def search(self, word: str) -> bool:
+        cur = self.hm
+        for c in word:
+            if c not in cur:
+                return False
+            cur = cur[c]
+        return "." in cur
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.hm
+        for c in prefix:
+            if c not in cur:
+                return False
+            cur = cur[c]
+        return True
