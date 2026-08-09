f1d1acb0a8ba66a2b6e28c1f0783ebbc867acaf4
Jaem Dessources <jaem.developer@gmail.com>
2026-08-09 15:42:50 -0400

    Add: search-for-word - submission-4

---
+++ Data Structures & Algorithms/search-for-word/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        n, m = len(board), len(board[0])
+        delta_x, delta_y = [1,0,-1,0], [0,1,0,-1]
+        seen = set()
+        def recurse(index, i, j):
+            if index == len(word):
+                return True
+            seen.add((i,j))
+            for k in range(len(delta_x)):
+                x, y = delta_x[k]+i, delta_y[k]+j
+                if x < n and x >= 0 and y < m and y >= 0 and (x,y) not in seen:
+                    if board[x][y] == word[index] and recurse(index+1, x, y):
+                        return True
+            seen.remove((i,j))
+            return False
+
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == word[0]:
+                    if recurse(1, i, j):
+                        return True
+        return False
+        
