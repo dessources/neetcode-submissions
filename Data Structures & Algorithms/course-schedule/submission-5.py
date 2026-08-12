b54cf8b67aa0e1b9b5fdf666b73196da838620f9
Jaem Dessources <jaem.developer@gmail.com>
2026-08-11 21:42:19 -0400

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if not prerequisites:
+            return True
+
+        hm = defaultdict(set)
+        seen = set()
+        checked = [False]*numCourses
+
+        for item in prerequisites:
+            course, prereq = item[0], item[1]
+            hm[course].add(prereq)
+
+        # @cache
+        def dfs(course):
+            if course in seen:
+                return False
+            if checked[course]:
+                return True
+
+            seen.add(course)
+            if course in hm:
+                for prereq in hm[course]:
+                    if not dfs(prereq):
+                        return False
+            seen.remove(course)
+            return True
+
+        for course in hm:
+            if not dfs(course):
+                return False
+            checked[course] = True
+
+        return True
