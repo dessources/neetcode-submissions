ddeb5664f7e54c18b95bf6f1170abc3ce5c00a61
Jaem Dessources <jaem.developer@gmail.com>
2026-08-13 11:07:43 -0400

    Add: valid-tree - submission-9

---
+++ Data Structures & Algorithms/valid-tree/submission-9.py
@@ -0,0 +1,25 @@
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if not edges:
+            return True
+
+        hm = {i:[] for i in range(n)}
+
+        for  a,b in edges:
+            hm[a].append(b)
+            hm[b].append(a)
+       
+        seen = set()
+        def dfs(node, parent):
+            if node in seen:
+                return False
+            
+            seen.add(node)
+            if node in hm:
+                for neigh in hm[node]:
+                    if neigh != parent and not dfs(neigh,node):
+                        return False
+            return True
+
+        return dfs(0, None) and n == len(seen)
