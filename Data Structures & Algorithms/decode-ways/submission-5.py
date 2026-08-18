53dc5c9920cbaf31a99b31cee6e9984ba727bb7a
Jaem Dessources <jaem.developer@gmail.com>
2026-08-17 21:55:00 -0400

    Add: decode-ways - submission-5

---
+++ Data Structures & Algorithms/decode-ways/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        dp = [0] * n
+        dp[-1] = 1
+
+        def backtrack(i):
+            if i >= n:
+                return 1
+            if s[i] == "0":
+                return 0
+       
+            if not dp[i]:
+                dp[i] = backtrack(i+1)
+
+                if i < n-1 and (s[i] == "1" or s[i]=="2" and int(s[i+1]) in range(0,7)):
+                    dp[i] += backtrack(i+2)
+            return dp[i]
+        
+        return backtrack(0)
+        
+
+
+        
