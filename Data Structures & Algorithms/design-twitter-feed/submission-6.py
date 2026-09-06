6878d9be1a63779c6db20e9ee914d81facc7f6e9
Jaem Dessources <jaem.developer@gmail.com>
2026-09-05 23:31:50 -0400

    Add: design-twitter-feed - submission-6

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-6.py
@@ -0,0 +1,38 @@
+class Twitter:
+
+    def __init__(self):
+        self.users = defaultdict(set)
+        self.posts = defaultdict(list)
+        self.time = 0
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        posts = self.posts[userId]
+        posts.append([-self.time, tweetId, userId])
+        self.time += 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        ids = [userId] + list(self.users[userId])
+        heap = [self.posts[i][-1]+[1] for i in ids if self.posts[i]]
+
+        heapq.heapify(heap)
+        result = []
+        while heap and len(result) < 10:
+            _, postId, user, rank = heapq.heappop(heap)
+            result.append(postId)
+            if rank+1 <= len(self.posts[user]):
+                new_post = self.posts[user][-(rank+1)]+[rank+1]
+                heapq.heappush(heap, new_post)
+
+        return result
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId == followeeId:
+            return
+        self.users[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.users[followerId]:
+            self.users[followerId].remove(followeeId)
+   
+
+        
