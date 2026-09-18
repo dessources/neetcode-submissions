2a9f6b835c0f8bd6807b2b451861aa6f490aead1
Jaem Dessources <jaem.developer@gmail.com>
2026-09-17 23:53:55 -0400

    Add: swim-in-rising-water - submission-7

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-7.py
@@ -0,0 +1,25 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        last = (ROWS-1, ROWS-1)
+
+        seen = set()
+        heap = [(grid[0][0], (0, 0))]
+        # seen.add((0, 0))
+        while heap:
+            time, (i, j) = heapq.heappop(heap)
+            if (i, j) in seen:
+                continue
+
+            seen.add((i, j))
+            if (i, j) == last:
+                return time
+
+            for di, dj in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+                x, y = i+di, j+dj
+                if x < 0 or x == ROWS or y < 0 or y == COLS or (x, y) in seen:
+                    continue
+                # seen.add((x, y))
+                new_time = max(time, grid[x][y])
+
+                heapq.heappush(heap, (new_time, (x, y)))
